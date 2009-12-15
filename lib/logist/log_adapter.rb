6e714dc3df8c6436abd2ad94609b9ff685430177
Westward Web Development <westwardwd@westwardwd-mbl1.local>
2009-12-15 06:23:46 -0500

    Added full factory functionality to Logist::LogAdapter
    Use Logist::LogAdapter.build(name) to build an adpater subclass
    This will create the factory method Logist::LogAdapter.name_adapter to return an instance
    of the subclass

--- lib/logist/log_adapter.rb
+++ lib/logist/log_adapter.rb
@@ -1,5 +1,27 @@
 module Logist
   class LogAdapter
+    @@adapters = {}
+
+    class << self
+      def adapters
+        @@adapters
+      end
+
+      def build(sym, superclass = Logist::LogAdapter, &block)
+        subclass = Class.new(superclass, &block)
+        subclass.register_adapter(sym)
+        Logist.const_set("#{sym.to_s.capitalize}LogAdapter", subclass)
+        (class << self; self; end).instance_eval do
+          define_method("#{sym.to_s}_adapter".intern) { subclass.new }
+        end
+      end
+
+      protected
+        def register_adapter(sym)
+          @@adapters[sym] = self
+        end
+    end
+    
     def parse_entry(entry = "")
       return nil unless valid_entry?(entry)
       entry
@@ -8,5 +30,6 @@ module Logist
     def valid_entry?(entry = "")
       not entry.empty?
     end
+    
   end
 end
--- spec/logist/log_adapter_spec.rb
+++ spec/logist/log_adapter_spec.rb
@@ -2,9 +2,30 @@ require File.join(File.dirname(__FILE__), %w[.. spec_helper])
 module Logist
   describe LogAdapter do
     context "Acts as factory" do
-      it "should return an instance of itself when not given a format" do
+      it "should return an instance of itself when new is called" do
         adapter = LogAdapter.new
-        adapter.should be_instance_of(LogAdapter)
+        adapter.should be_instance_of(Logist::LogAdapter)
+      end
+      it "should provide a method for building subclasses" do
+        LogAdapter.build(:test)
+        subclass = nil
+        lambda do
+          subclass = Logist.const_get(:TestLogAdapter)
+        end.should_not raise_error
+        subclass.should == Logist::TestLogAdapter
+      end
+      it "should add a class factory method named the same as the symbol" do
+        LogAdapter.build(:test)
+        adapter = LogAdapter.test_adapter
+        adapter.should be_kind_of(Logist::TestLogAdapter)
+      end
+      it "should allow a block to passed to the build method so instance methods of the subclass can be defined" do
+        LogAdapter.build(:test) do
+          def test_method
+          end
+        end
+        adapter = LogAdapter.test_adapter
+        adapter.should respond_to(:test_method)
       end
     end
     context "entries" do
