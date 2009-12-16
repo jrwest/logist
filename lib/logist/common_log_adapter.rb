6f35fe71c7039e9ef376e57e031485929c207735
Jordan West <jordan.west@westwardwd.com>
2009-12-16 18:24:19 -0500

    Common Log Adapter converts empty entries (-) to nil, handles missing HTTP protocol and returns nil for invalid entries when parsing entries.
    Completed both pending specs and added spec for should return nil if entry is not valid.
    
    Logist::CommonLogAdapter
      instantiation
        should be instantiated through the factory method common_adapter
      entries
        #valid_entry?
          should return true if the entry is valid common log format
          should return false if the entry is not in valid common log format
        parsing entries
          should return an instance of Logist:Entry if valid
          should correctly parse all fields and additionally merge the datetime fields in a timestamp fiend
          should create the timestamp field in a format that Entry can convert to a non nil instance
          should convert emtpy entries (-) to nil
          should return nil if the entry is not valid
          should be able to handle an entry with no protocol, setting to nil

--- lib/logist/common_log_adapter.rb
+++ lib/logist/common_log_adapter.rb
@@ -3,14 +3,17 @@ Logist::LogAdapter.build(:common) do
     entry =~ /^(.+) (.+|-) (.+|-) \[(\d{2})\/([A-z][a-z]{2})\/(\d{4}):(\d{2}):(\d{2}):(\d{2}) (\+\d{4}|-\d{4})\] "([A-Z]{3,}) (\/.*)( HTTP\/?\d?\.?\d?)?" (\d{3}) (\d*)$/ 
   end
 
-  def parse_entry(entry = '') 
+  def parse_entry(entry = '')
+    return unless valid_entry?(entry)
+    
     field_data = entry.match(/^(.+) (.+|-) (.+|-) \[(\d{2})\/([A-z][a-z]{2})\/(\d{4}):(\d{2}):(\d{2}):(\d{2}) (\+\d{4}|-\d{4})\] "([A-Z]{3,}) (\/\S*)( HTTP\/?\d?\.?\d?)?" (\d{3}) (\d*)$/)
     entry_data = {}
     fields = [:client, :rfc1413, :userid, :day, :month, :year, :hour, :minute, :second, :tzn, :httpmethod, :resource, :protocol, :status_code, :response_size, :timestamp]
     field_data[1..field_data.size - 1].each_with_index do |field, index|
-      entry_data[fields[index]] = field.strip
+      entry_data[fields[index]] = (!field.nil? && field != '-') ? field.strip : nil
     end
     entry_data[:timestamp] = "#{entry_data[:day]}/#{entry_data[:month]}/#{entry_data[:year]}:#{entry_data[:hour]}:#{entry_data[:minute]}:#{entry_data[:second]} #{entry_data[:tzn]}"
+
     Logist::Entry.new(entry_data)
   end
 end
--- spec/logist/common_log_adapter_spec.rb
+++ spec/logist/common_log_adapter_spec.rb
@@ -70,10 +70,61 @@ module Logist
          entry.timestamp.should_not be_nil
        end
        it "should convert emtpy entries (-) to nil" do
-         pending
+          Entry.should_receive(:new).with({:client => 'client',
+                                          :rfc1413 => nil,
+                                          :userid => nil,
+                                          :day => '01',
+                                          :month => 'Jan',
+                                          :year => '2010',
+                                          :hour => '00',
+                                          :minute => '00',
+                                          :second => '00',
+                                          :tzn => '+0400',
+                                          :httpmethod => 'GET',
+                                          :resource => '/a.html',
+                                          :protocol => 'HTTP/1.1',
+                                          :status_code => '200',
+                                          :response_size => '1493',
+                                          :timestamp => '01/Jan/2010:00:00:00 +0400'})
+         adapter = LogAdapter.common_adapter
+         adapter.parse_entry('client - - [01/Jan/2010:00:00:00 +0400] "GET /a.html HTTP/1.1" 200 1493')
        end
-       it "should be able to handle an entry with no protocol" do
-        pending
+       it "should return nil if the entry is not valid" do
+         adapter = LogAdapter.common_adapter
+         invalid_entries = [
+           '- - [01/Dec/2009:00:00:00] "GET /a.html HTTP/1.1" 200 1234',
+           '127.0.0.1 - [01/Dec/2009:00:00:00 -0400] "GET /a.html HTTP/1.1" 200 1234',
+           'my.host.name - - [01/Dec/2009:00:00 -0400] "GET /a.html HTTP/1.1" 200 1234',
+           'my.host.name - - [01/Dec:00:00:00 -0400] "GET /a.html HTTP/1.1" 200 1234',
+           'my.host.name - - [01/Dec/2010:00:00:00] "GET /a.html HTTP/1.1" 200 1234',
+           'my.host.name - - [01/Dec/2009:00:00:00] "/a.html HTTP/1.1" 200 1234',
+           'my.host.name - - [01/Dec/2009:00:00:00] "GET HTTP/1.1" 200 1234',
+           'my.host.name - - [01/Dec/2009:00:00:00] "GET /a.html HTTP/1.1" nan 1234',
+           'my.host.name - - [01/Dec/2009:00:00:00] "GET /a.html HTTP/1.1" 200 nan'
+         ]
+         invalid_entries.each do |entry| 
+           adapter.parse_entry(entry).should be_nil  
+         end
+       end
+       it "should be able to handle an entry with no protocol, setting to nil" do
+        Entry.should_receive(:new).with({:client => '172.93.45.2',
+                                          :rfc1413 => nil,
+                                          :userid => nil,
+                                          :day => '01',
+                                          :month => 'Jan',
+                                          :year => '2009',
+                                          :hour => '12',
+                                          :minute => '00',
+                                          :second => '00',
+                                          :tzn => '+0530',
+                                          :httpmethod => 'GET',
+                                          :resource => '/images/b.gif',
+                                          :protocol => nil,
+                                          :status_code => '302',
+                                          :response_size => '193',
+                                          :timestamp => '01/Jan/2009:12:00:00 +0530'})
+         adapter = LogAdapter.common_adapter
+         adapter.parse_entry('172.93.45.2 - - [01/Jan/2009:12:00:00 +0530] "GET /images/b.gif" 302 193')
        end
      end
    end
