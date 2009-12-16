d0e82a800cfec20920f6a7fbf9f664a155e4e8f4
Jordan West <jordan.west@westwardwd.com>
2009-12-15 20:19:47 -0500

    Logist::CommonLogAdapter#parse_entry peroforms basic parsing and returns instance of Logist::Entry
    Added first spec for parsing log entries. Also modified valid entries to include timezones and for Logist::CommonLogAdapter#valid_entry? to require them.
    
    Specs:
    
    Logist::CommonLogAdapter
      instantiation
        should be instantiated through the factory method common_adapter
      entries
        #valid_entry?
          should return true if the entry is valid common log format
          should return false if the entry is not in valid common log format
        parsing entries
          should return an instance of Logist:Entry if valid

--- lib/logist/common_log_adapter.rb
+++ lib/logist/common_log_adapter.rb
@@ -1,5 +1,15 @@
 Logist::LogAdapter.build(:common) do
   def valid_entry?(entry = '') 
-    entry =~ /^(.+) (.+|-) (.+|-) \[(\d{2})\/([A-z][a-z]{2})\/(\d{4}):(\d{2}):(\d{2}):(\d{2})\] "([A-Z]{3,}) (\/.*)( HTTP\/?\d?\.?\d?)?" (\d{3}) (\d*)$/ 
+    entry =~ /^(.+) (.+|-) (.+|-) \[(\d{2})\/([A-z][a-z]{2})\/(\d{4}):(\d{2}):(\d{2}):(\d{2}) (\+\d{4}|-\d{4})\] "([A-Z]{3,}) (\/.*)( HTTP\/?\d?\.?\d?)?" (\d{3}) (\d*)$/ 
+  end
+
+  def parse_entry(entry = '') 
+    field_data = entry.match(/^(.+) (.+|-) (.+|-) \[(\d{2})\/([A-z][a-z]{2})\/(\d{4}):(\d{2}):(\d{2}):(\d{2}) (\+\d{4}|-\d{4})\] "([A-Z]{3,}) (\/\S*)( HTTP\/?\d?\.?\d?)?" (\d{3}) (\d*)$/)
+    entry_data = {}
+    fields = [:client, :rfc1413, :userid, :day, :month, :year, :hour, :minute, :second, :tzn, :httpmethod, :resource, :protocol, :status_code, :response_size, :timestamp]
+    field_data[1..field_data.size - 1].each_with_index do |field, index|
+      entry_data[fields[index]] = field
+      end
+    Logist::Entry.new(entry_data)
   end
 end
--- spec/logist/common_log_adapter_spec.rb
+++ spec/logist/common_log_adapter_spec.rb
@@ -12,10 +12,10 @@ module Logist
        it "should return true if the entry is valid common log format" do
          adapter = LogAdapter.common_adapter
          valid_entries = [
-           'client rfc1413 userid [01/Jan/2010:00:00:00] "GET /a.html HTTP/1.1" 200 1493',
-           '172.93.45.2 - - [01/Jan/2009:12:00:00] "GET /images/b.gif" 302 193',
-           '172.93.45.2 - - [02/Feb/2009:12:00:00] "GET /images/c.gif HTTP/1.1" 200 193',
-           'my.host.name - - [02/Mar/2010:14:30:21] "POST /services HTTP/1.1/" 200 1493'
+           'client rfc1413 userid [01/Jan/2010:00:00:00 -0400] "GET /a.html HTTP/1.1" 200 1493',
+           '172.93.45.2 - - [01/Jan/2009:12:00:00 +0530] "GET /images/b.gif" 302 193',
+           '172.93.45.2 - - [02/Feb/2009:12:00:00 -0400] "GET /images/c.gif HTTP/1.1" 200 193',
+           'my.host.name - - [02/Mar/2010:14:30:21 -0400] "POST /services HTTP/1.1/" 200 1493'
          ]
          valid_entries.each do |entry|
            adapter.should be_valid_entry(entry)
@@ -25,9 +25,10 @@ module Logist
          adapter = LogAdapter.common_adapter
          invalid_entries = [
            '- - [01/Dec/2009:00:00:00] "GET /a.html HTTP/1.1" 200 1234',
-           '127.0.0.1 - [01/Dec/2009:00:00:00] "GET /a.html HTTP/1.1" 200 1234',
-           'my.host.name - - [01/Dec/2009:00:00] "GET /a.html HTTP/1.1" 200 1234',
-           'my.host.name - - [01/Dec:00:00:00] "GET /a.html HTTP/1.1" 200 1234',
+           '127.0.0.1 - [01/Dec/2009:00:00:00 -0400] "GET /a.html HTTP/1.1" 200 1234',
+           'my.host.name - - [01/Dec/2009:00:00 -0400] "GET /a.html HTTP/1.1" 200 1234',
+           'my.host.name - - [01/Dec:00:00:00 -0400] "GET /a.html HTTP/1.1" 200 1234',
+           'my.host.name - - [01/Dec/2010:00:00:00] "GET /a.html HTTP/1.1" 200 1234',
            'my.host.name - - [01/Dec/2009:00:00:00] "/a.html HTTP/1.1" 200 1234',
            'my.host.name - - [01/Dec/2009:00:00:00] "GET HTTP/1.1" 200 1234',
            'my.host.name - - [01/Dec/2009:00:00:00] "GET /a.html HTTP/1.1" nan 1234',
@@ -36,6 +37,12 @@ module Logist
          invalid_entries.each do |entry|
            adapter.should_not be_valid_entry(entry)
          end
+       end
+     end
+     context "parsing entries" do
+       it "should return an instance of Logist:Entry if valid" do
+         adapter = LogAdapter.common_adapter
+         adapter.parse_entry('client rfc1413 userid [01/Jan/2010:00:00:00 +0400] "GET /a.html HTTP/1.1" 200 1493').should be_instance_of(Logist::Entry)
        end
      end
    end
