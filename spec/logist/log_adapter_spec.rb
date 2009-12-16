16079f68be8a8861f7de0b4fcab077d61affd0e7
Jordan West <jordan.west@westwardwd.com>
2009-12-16 18:35:35 -0500

    Re-factored small piece of log adapter spec

--- spec/logist/log_adapter_spec.rb
+++ spec/logist/log_adapter_spec.rb
@@ -29,25 +29,24 @@ module Logist
       end
     end
     context "entries" do
+      before(:each) do
+        @adapter = LogAdapter.new
+      end
       context "#valid_entry?" do
         it "should return true if entry is not an emtpy string" do
-          adapter = LogAdapter.new
-          adapter.should be_valid_entry('not empty')
+          @adapter.should be_valid_entry('not empty')
         end
         it "should return false if entry is empty string (which is default argument value)" do
-          adapter = LogAdapter.new
-          adapter.should_not be_valid_entry
-          adapter.should_not be_valid_entry('')
+          @adapter.should_not be_valid_entry
+          @adapter.should_not be_valid_entry('')
         end
       end
       context "#parse_entry" do
         it "should return the same string passed if valid entry" do
-          adapter = LogAdapter.new
-          adapter.parse_entry("entry").should == "entry"
+          @adapter.parse_entry("entry").should == "entry"
         end
         it "should return nil if entry is invalid" do
-          adapter = LogAdapter.new
-          adapter.parse_entry("").should be_nil
+          @adapter.parse_entry("").should be_nil
         end
       end
     end
