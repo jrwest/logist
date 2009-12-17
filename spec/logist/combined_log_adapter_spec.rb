519b7ca25e6c9fd405395299f140c6e7c97970c6
Jordan West <jordan.west@westwardwd.com>
2009-12-17 03:12:15 -0500

    CombinedLogAdapter correctly handles combined and common log format entries
    Logist::CombinedLogAdapter
      instantiation
        should be instantiated through factory method combined_adapter
      entries
        #valid_entry?
          should return true if the entry is in valid combined log format
          should return false if the entry is not in valid combined log format
        parsing entries
          should correctly parse all fields and additionally merge the datetime fields in a timestamp field
          should return nil if the entry is invalid
          should correctly handle missing cookies field, setting values to nil
          should correctly handle missing cookies and user agent field, setting values to nil
          should correctly handle missing trailing fields (in common log format), setting values to nil
          should correctly handle missing HTTP protocol, setting value to nil

--- spec/logist/combined_log_adapter_spec.rb
+++ spec/logist/combined_log_adapter_spec.rb
@@ -2,10 +2,12 @@ require File.join(File.dirname(__FILE__), %w[.. spec_helper])
 
 module Logist
   describe CombinedLogAdapter do
+    before(:each) do
+      @adapter = LogAdapter.combined_adapter
+    end
     context "instantiation" do
       it "should be instantiated through factory method combined_adapter" do
-        adapter = LogAdapter.combined_adapter
-        adapter.should be_instance_of(Logist::CombinedLogAdapter)
+        @adapter.should be_instance_of(Logist::CombinedLogAdapter)
       end
     end
     context "entries" do
@@ -21,9 +23,8 @@ module Logist
                     '127.0.0.1 - - [01/Jan/2010:00:00:00 -0400] "GET /a.html HTTP/1.1" 200 1493 "referrer" "user agent" ""',
                     '127.0.0.1 - - [01/Jan/2010:00:00:00 -0400] "GET /a.gif" 200 123 "http://www.referrer.com" "user agent" "cookies"'
                   ]
-          adapter = LogAdapter.combined_adapter
           valid_entries.each do |entry|
-            adapter.should be_valid_entry(entry)
+            @adapter.should be_valid_entry(entry)
           end
         end
         it "should return false if the entry is not in valid combined log format" do
@@ -37,10 +38,125 @@ module Logist
                     '127.0.0.1 - - [01/Jan/2010:00:00:00 -0400] "GET /a.html HTTP/1.1" 200 1493 "referrer" "user agent" "',   #no last closing double quote
                     '127.0.0.1 - - [01/Jan/2010:00:00:00 -0400] "GET /a.gif" 123 "http://www.referrer.com" "user agent" "cookies"'
                   ]
-          adapter = LogAdapter.combined_adapter
           invalid_entries.each do |entry|
-            adapter.should_not be_valid_entry(entry)
+            @adapter.should_not be_valid_entry(entry)
           end
+        end
+      end
+      context "parsing entries" do
+        it "should correctly parse all fields and additionally merge the datetime fields in a timestamp field" do
+          Entry.should_receive(:new).with({:client => 'client',
+                                            :rfc1413 => 'rfc1413',
+                                            :userid => 'userid',
+                                            :day => '01',
+                                            :month => 'Jan',
+                                            :year => '2010',
+                                            :hour => '00',
+                                            :minute => '00',
+                                            :second => '00',
+                                            :tzn => '+0400',
+                                            :httpmethod => 'GET',
+                                            :resource => '/a.html',
+                                            :protocol => 'HTTP/1.1',
+                                            :status_code => '200',
+                                            :response_size => '1493',
+                                            :referrer => 'referrer',
+                                            :user_agent => 'user agent',
+                                            :cookies => 'cookies',
+                                            :timestamp => '01/Jan/2010:00:00:00 +0400'})
+           @adapter.parse_entry('client rfc1413 userid [01/Jan/2010:00:00:00 +0400] "GET /a.html HTTP/1.1" 200 1493 "referrer" "user agent" "cookies"')
+        end
+        it "should return nil if the entry is invalid" do
+          adapter = LogAdapter.combined_adapter
+          adapter.parse_entry('invalid').should be_nil
+        end
+        it "should correctly handle missing cookies field, setting values to nil" do
+          Entry.should_receive(:new).with({:client => 'client',
+                                            :rfc1413 => 'rfc1413',
+                                            :userid => 'userid',
+                                            :day => '01',
+                                            :month => 'Jan',
+                                            :year => '2010',
+                                            :hour => '00',
+                                            :minute => '00',
+                                            :second => '00',
+                                            :tzn => '+0400',
+                                            :httpmethod => 'GET',
+                                            :resource => '/a.html',
+                                            :protocol => 'HTTP/1.1',
+                                            :status_code => '200',
+                                            :response_size => '1493',
+                                            :referrer => 'referrer',
+                                            :user_agent => 'user agent',
+                                            :cookies => nil,
+                                            :timestamp => '01/Jan/2010:00:00:00 +0400'})
+           @adapter.parse_entry('client rfc1413 userid [01/Jan/2010:00:00:00 +0400] "GET /a.html HTTP/1.1" 200 1493 "referrer" "user agent"')
+        end
+        it "should correctly handle missing cookies and user agent field, setting values to nil" do
+          Entry.should_receive(:new).with({:client => 'client',
+                                            :rfc1413 => 'rfc1413',
+                                            :userid => 'userid',
+                                            :day => '01',
+                                            :month => 'Jan',
+                                            :year => '2010',
+                                            :hour => '00',
+                                            :minute => '00',
+                                            :second => '00',
+                                            :tzn => '+0400',
+                                            :httpmethod => 'GET',
+                                            :resource => '/a.html',
+                                            :protocol => 'HTTP/1.1',
+                                            :status_code => '200',
+                                            :response_size => '1493',
+                                            :referrer => 'referrer',
+                                            :user_agent => nil,
+                                            :cookies => nil,
+                                            :timestamp => '01/Jan/2010:00:00:00 +0400'})
+           @adapter.parse_entry('client rfc1413 userid [01/Jan/2010:00:00:00 +0400] "GET /a.html HTTP/1.1" 200 1493 "referrer"')
+        end
+        it "should correctly handle missing trailing fields (in common log format), setting values to nil" do
+          Entry.should_receive(:new).with({:client => 'client',
+                                            :rfc1413 => 'rfc1413',
+                                            :userid => 'userid',
+                                            :day => '01',
+                                            :month => 'Jan',
+                                            :year => '2010',
+                                            :hour => '00',
+                                            :minute => '00',
+                                            :second => '00',
+                                            :tzn => '+0400',
+                                            :httpmethod => 'GET',
+                                            :resource => '/a.html',
+                                            :protocol => 'HTTP/1.1',
+                                            :status_code => '200',
+                                            :response_size => '1493',
+                                            :referrer => nil,
+                                            :user_agent => nil,
+                                            :cookies => nil,
+                                            :timestamp => '01/Jan/2010:00:00:00 +0400'})
+           @adapter.parse_entry('client rfc1413 userid [01/Jan/2010:00:00:00 +0400] "GET /a.html HTTP/1.1" 200 1493')
+        end
+        it "should correctly handle missing HTTP protocol, setting value to nil" do
+           Entry.should_receive(:new).with({:client => 'client',
+                                            :rfc1413 => 'rfc1413',
+                                            :userid => 'userid',
+                                            :day => '01',
+                                            :month => 'Jan',
+                                            :year => '2010',
+                                            :hour => '00',
+                                            :minute => '00',
+                                            :second => '00',
+                                            :tzn => '+0400',
+                                            :httpmethod => 'GET',
+                                            :resource => '/a.html',
+                                            :protocol => nil,
+                                            :status_code => '200',
+                                            :response_size => '1493',
+                                            :referrer => 'referrer',
+                                            :user_agent => 'user agent',
+                                            :cookies => 'cookies',
+                                            :timestamp => '01/Jan/2010:00:00:00 +0400'})
+           @adapter.parse_entry('client rfc1413 userid [01/Jan/2010:00:00:00 +0400] "GET /a.html" 200 1493 "referrer" "user agent" "cookies"')  
         end
       end
     end
