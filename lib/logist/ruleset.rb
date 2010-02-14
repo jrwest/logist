240daa65f50f2612a2df6fdc72064706fdc6ba73
Jordan West, Westward Web Development <jordan.west@westwardwd.com>
2010-02-14 00:46:51 -0500

    added basic ruleset functionality: a ruleset has a name which is parsed into a prettier format and a set of rules which an entry must conform to
    an entry is checked again each rule using the Rule#met_by? method.

--- lib/logist/ruleset.rb
+++ lib/logist/ruleset.rb
@@ -1,4 +1,21 @@
 module Logist
   class RuleSet
+    attr_reader :name, :rules
+    
+    def initialize(name, rules = [])
+      @name = name.to_s.split("_").map do |word|
+        word.capitalize
+      end.join(" ")
+      @rules = rules
+    end
+    
+    def conformed?(entry)
+      rules.each do |rule|
+        unless rule.met_by?(entry)
+          return false
+        end
+      end
+      true
+    end
   end
 end
--- spec/logist/ruleset_spec.rb
+++ spec/logist/ruleset_spec.rb
@@ -2,11 +2,43 @@ require File.join(File.dirname(__FILE__), %w[.. spec_helper])
 
 module Logist
   describe RuleSet do
-    it "has a name"
-    it "has a list of rules"
+    before(:each) do
+      @rset = RuleSet.new(:my_ruleset) 
+    end
+    it "has a name" do 
+      @rset.name.should == "My Ruleset"
+    end
+    it "has a list of rules" do
+      some_rule = mock(Rule)
+      another_rset = RuleSet.new(:my_ruleset, [some_rule])
+      @rset.rules.should == []
+      another_rset.rules.should == [some_rule]
+    end
     context "checking agains an entry" do
-      it "returns true if an entry conforms to the ruleset"
-      it "returns false if an entry does not conform to a ruleset"
+      let(:entry) { mock(Entry).as_null_object }
+      let(:rule1) { mock(Rule).as_null_object }
+      let(:rule2) { mock(Rule).as_null_object }
+      before(:each) do
+        @rset = RuleSet.new(:my_ruleset, [rule1, rule2])
+      end
+      context "when each entry meets each rule" do
+        before(:each) do
+          @rset.rules.each do |rule|
+            rule.stub!(:met_by?).and_return(true)
+          end
+        end
+        it "returns true if an entry conforms to the ruleset" do
+          @rset.should be_conformed(entry)
+        end
+      end
+      context "when an entry does not meet a rule" do
+        before(:each) do
+          rule2.stub!(:met_by?).and_return(false)
+        end
+        it "returns false if an entry does not conform to a ruleset" do
+          @rset.should_not be_conformed(entry)
+        end
+      end
     end
   end
 end
